8cd9a6950e2797e24108430edb71ea5e06014450
mekaaku <meekaaku@gmail.com>
2024-01-06 23:31:04 +0000

    Compoments and icons added

---
+++ src/lib/ui/SidebarLink.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+export let name = '';
+export let url = '';
+export let icon = '';
+
+</script>
+
+<style>
+.sidebar-item 
+{
+    font-size: 90%;
+}
+</style>
+
+<li class="sidebar-item">
+    <a class="sidebar-link" href={url}> <i data-feather="feather"></i> {name}</a>
+</li>
--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -19,6 +19,7 @@ function toggle()
 	{
 		navbarElement.classList.add("navbar-narrow");
 	}
+	feather.replace();
 }
 
 onMount(toggle);
@@ -85,6 +86,10 @@ main {
 						<UI.SidebarLink name="Products" url="/" />
 					</UI.SidebarItem>
 
+					<UI.SidebarItem icon="feather" name="Sales">
+						<UI.SidebarLink name="Orders" url="/" />
+						<UI.SidebarLink name="Customers" url="/" />
+					</UI.SidebarItem>
 
 					<li class="sidebar-item">
 						<a data-bs-target="#catalog" data-bs-toggle="collapse" class="sidebar-link collapsed">
@@ -448,9 +453,9 @@ main {
 			<main class="content">
 				<div class="container-fluid p-0">
 
-					<h1 class="h3 mb-3"><strong>Analytics</strong> Dashboard</h1>
+					<slot />
 
-					<div class="row">
+					<div class="row d-none">
 						<div class="col-xl-6 col-xxl-5 d-flex">
 							<div class="w-100">
 								<div class="row">
@@ -557,7 +562,7 @@ main {
 						</div>
 					</div>
 
-					<div class="row">
+					<div class="row d-none">
 						<div class="col-12 col-md-6 col-xxl-3 d-flex order-2 order-xxl-3">
 							<div class="card flex-fill w-100">
 								<div class="card-header">
@@ -620,7 +625,7 @@ main {
 						</div>
 					</div>
 
-					<div class="row">
+					<div class="row d-none">
 						<div class="col-12 col-lg-8 col-xxl-9 d-flex">
 							<div class="card flex-fill">
 								<div class="card-header">
