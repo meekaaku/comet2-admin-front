85b37db9117d33c3af516b6c421fbd7a5182271e
mekaaku <meekaaku@gmail.com>
2024-01-07 14:16:37 +0000

    Login page done

--- src/routes/app/sales/orders/+page.svelte
+++ src/routes/app/sales/orders/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-    import * as UI from '$lib/ui';
+import * as UI from '$lib/ui';
+
+
+
 </script>
 
 <UI.Title>Sales Orders</UI.Title>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,13 +1,27 @@
 <script lang="ts">
+import { goto } from '$app/navigation';
 import { comet } from '$lib'
 
 let tenant_username = '';
 let user_username = '';
 let user_password = '';
+let error_data: any|null = null;
+
 async function login()
 {
-	const token = await comet.auth.login({tenant_username, user_username, user_password})
-	console.log({token});
+	try 
+	{
+		error_data = null;
+		const { token } = await comet.auth.login({tenant_username, user_username, user_password})
+		comet.setHeader('Authorization', `Bearer ${token}`);
+		goto('/app');
+
+	}
+	catch (e: any)
+	{
+		console.log(e);
+		error_data = e.response.data;
+	}
 }
 </script>
 <main class="d-flex w-100">
@@ -17,7 +31,7 @@ async function login()
 					<div class="d-table-cell align-middle">
 
 						<div class="text-center mt-4">
-							<h1 class="h2">Admin</h1>
+							<h1 class="h2">Login</h1>
 							<p class="lead">
 								Sign in to your account to continue
 							</p>
@@ -26,20 +40,20 @@ async function login()
 						<div class="card">
 							<div class="card-body">
 								<div class="m-sm-3">
-									<form>
-										<div class="mb-3">
+									<form on:submit|preventDefault={login}>
+										<div class="mb-3 form-floating">
+											<input class="form-control form-control-lg" bind:value={tenant_username} type="text" name="tenant_username" placeholder=" You Organisation" />
 											<label class="form-label">Organisation</label>
-											<input class="form-control form-control-lg" bind:value={tenant_username} type="text" name="tenant_username" placeholder="Organisation" />
 										</div>									
 
 
-										<div class="mb-3">
-											<label class="form-label">Username</label>
+										<div class="mb-3 form-floating">
 											<input class="form-control form-control-lg" bind:value={user_username} type="text" name="username" placeholder="Enter your username" />
+											<label class="form-label">Username</label>
 										</div>
-										<div class="mb-3">
-											<label class="form-label">Password</label>
+										<div class="mb-3 form-floating">
 											<input class="form-control form-control-lg" bind:value={user_password} type="password" name="password" placeholder="Enter your password" />
+											<label class="form-label">Password</label>
 										</div>
 										<div>
 											<div class="form-check align-items-center d-none">
@@ -48,12 +62,23 @@ async function login()
 											</div>
 										</div>
 										<div class="d-grid gap-2 mt-3">
-											<a href="#" class="btn btn-lg btn-primary" on:click={login}>Login</a>
+											<a href="#" class="btn btn-lg btn-primary d-none" on:click={login}>Login</a>
+											<button type="submit" class="btn btn-lg btn-primary">Login</button>
 										</div>
 									</form>
+								
+
+									{#if error_data}
+									<div class="text-center mb-3">
+										<span class="text-danger">{error_data.message}</span>
+									</div>
+									{/if}
+
 								</div>
 							</div>
 						</div>
+
+				
 						<div class="text-center mb-3 d-none">
 							Don't have an account? <a href="pages-sign-up.html">Sign up</a>
 						</div>
