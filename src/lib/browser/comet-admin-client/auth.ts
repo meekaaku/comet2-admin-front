554c23f2a8e4ee426484a87bc818fef7f020b3cf
mekaaku <meekaaku@gmail.com>
2024-01-10 10:50:16 +0000

    Work in progress

--- src/lib/browser/comet-admin-client/auth.ts
+++ src/lib/browser/comet-admin-client/auth.ts
@@ -4,8 +4,22 @@ import type { QLoginCredentials } from "./user.types";
 export class Auth {
 	constructor(private readonly client: any) {}
 
-	async login(credentials: QLoginCredentials) {
+	async login(credentials: QLoginCredentials) 
+	{
 		const response = await this.client.post('auth/user/browser', credentials);
-		return { token: response.data.token };
+		const token = response.data.token;
+		await this.setToken(token);
+		return { token };
+	}
+
+	async logout()
+	{
+		this.client.deleteHeader('Authorization');
+		localStorage.removeItem('token');
+	}
+
+	async setToken(token: string)
+	{
+		this.client.setHeader('Authorization', `Bearer ${token}`);
 	}
 }
--- src/lib/browser/comet-admin-client/orders.ts
+++ src/lib/browser/comet-admin-client/orders.ts
@@ -1,10 +1,14 @@
+import type { ROrderListRow, RPaginated } from "$lib/types";
 
 export class Orders
 {
 	constructor(private readonly client: any) {}
 
-	async list(filters:any = null) {
+	async list(filters:any = null): Promise<RPaginated<ROrderListRow>>
+	{
+		console.log('geting the orders');
 		const response = await this.client.get('sales/orders');
-		return { orders: response.data.orders };
+		console.log(response);
+		return response.data.orders as RPaginated<ROrderListRow>;
 	}
 }
