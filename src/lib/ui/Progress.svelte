1e25940e67158f190c974d7e2af7ee75c3d84196
mekaaku <meekaaku@gmail.com>
2024-01-15 22:25:39 +0000

    listing page

---
+++ src/lib/ui/Progress.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+import { navigating } from '$app/stores';
+import { progress, loading } from '$lib/stores'
+
+
+function check(n: any)
+{
+    const x = n;
+    if($navigating || $loading ){
+        if($progress < 50){
+            $progress += 2;
+        }
+        else if($progress < 75){
+            $progress += 1;
+        }
+        else if($progress < 95){
+            $progress += 0.5;
+        }
+        else {
+            $progress += 0;
+        }
+
+        //$progress += $progress < 50 ? 5 : 2;
+        if($progress >= 100){
+            $progress = 0;
+        }
+        else {
+            setTimeout(() => check(true), 100);
+        }
+        
+    }
+    else {
+        $progress = 0;
+    }
+}
+
+
+$: check($navigating)
+</script>
+
+
+{#if $progress != 0}
+<progress value={$progress} max="100"></progress>
+{/if}
+
+
+
+
+<style>
+progress {
+    position: fixed;
+    top: 0px;
+    height: 5px;
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    z-index: 3000;
+
+}
+
+
+@media only screen and (max-width: 600px) 
+{
+    progress {
+        top: 2.5em;
+    }
+}
+</style>
---
+++ src/lib/utils.ts
@@ -0,0 +1,68 @@
+import * as currency from 'currency.js';
+
+
+
+// Formats a number to user's locale
+export function formatNumber(n: number|string, decimals: number = 2) {
+	const formatter = new Intl.NumberFormat('en-EN', {minimumFractionDigits: 2, maximumFractionDigits: 2});
+	n = Number(n);
+	return formatter.format(n);
+}
+
+
+export function formatDate(str: string)
+{
+  const dt = new Date(str)
+  return dt.toLocaleDateString();
+}
+
+export function formatAddress(str: string)
+{
+  return str.replaceAll('\n', '<br />')
+}
+
+
+
+function toClassName(value: any) {
+  let result = '';
+
+  if (typeof value === 'string' || typeof value === 'number') {
+    result += value;
+  } else if (typeof value === 'object') {
+    if (Array.isArray(value)) {
+      result = value.map(toClassName).filter(Boolean).join(' ');
+    } else {
+      for (let key in value) {
+        if (value[key]) {
+          result && (result += ' ');
+          result += key;
+        }
+      }
+    }
+  }
+
+  return result;
+}
+
+export function classnames(...args: any[]) {
+  return args.map(toClassName).filter(Boolean).join(' ');
+}
+
+export function discountPercent(base: string|number, net: string|number): string
+{
+	return String(Math.round((1 - Number(net)/Number(base)) * 100));
+}
+
+export function replaceURLQuery(values: Record<string, string>)
+{
+  const url = new URL(window.location.toString());
+  for (let [k, v] of Object.entries(values)) {
+    if (!!v) {
+      url.searchParams.set(encodeURIComponent(k), encodeURIComponent(v));
+    } else {
+      url.searchParams.delete(k);
+    }
+  }
+  history.replaceState({}, '', url);
+};
+
