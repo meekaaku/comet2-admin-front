826dd98594847e144597881f807dd1349a68e2ac
meekaaku <meekaaku@gmail.com>
2024-06-01 17:21:57 +0500

    wip

---
+++ src/lib/ui/Toolbar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+
+</script>
+
+<style>
+
+.sticky-top {
+    background-color: white;
+    padding: 0.5em;
+    margin-bottom: 1em;
+}
+
+
+    
+</style>
+
+<div class="sticky-top shadow">
+    <slot></slot>
+</div>
+
+
+
