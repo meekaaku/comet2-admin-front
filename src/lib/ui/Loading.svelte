6ad3c370f9d67f3e5c43bb9ddee78da2f8298c12
meekaaku <meekaaku@gmail.com>
2024-06-02 17:11:59 +0500

    Loading empty state

---
+++ src/lib/ui/Loading.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+export let message = 'Loading...';
+</script>
+
+<main class="d-flex w-100">
+	<div class="container d-flex flex-column">
+		<div class="row" style="height: 80vh">
+			<div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 mx-auto d-table h-100">
+				<div class="d-table-cell align-middle">
+					<div class="text-center mt-4">
+                        <p>
+                            <span class="spinner-border" role="status" aria-hidden="true"></span>&nbsp; 
+                        </p>
+						<p class="lead">
+                            {message}
+						</p>
+					</div>
+
+
+				</div>
+			</div>
+		</div>
+	</div>
+</main>
