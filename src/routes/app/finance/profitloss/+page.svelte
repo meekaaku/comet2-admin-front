ff30c65868ec5bead589076ee0ab9cb7e63da21e
meekaaku <meekaaku@gmail.com>
2024-06-12 20:43:30 +0500

    Work in progress

--- src/routes/app/finance/profitloss/+page.svelte
+++ src/routes/app/finance/profitloss/+page.svelte
@@ -9,13 +9,18 @@ import { Title, Toolbar, Button } from '$lib/ui';
 
 let justMounted = false;
 let report: any = undefined;
+let date_from: string;
+let date_to: string;
+let group_by: string;
+
+
 
 async function load()
 {
     let query = $svpage.url.searchParams;
-    const date_from = query.get('date_from') || new Date().getFullYear() + '-01-01';
-    const date_to = query.get('date_to') || new Date().getFullYear() + '-12-31'; 
-    const group_by = query.get('group_by') || 'month';
+    date_from = query.get('date_from') || new Date().getFullYear() + '-01-01';
+    date_to = query.get('date_to') || new Date().getFullYear() + '-12-31'; 
+    group_by = query.get('group_by') || 'month';
     const spec = { date_from, date_to, group_by };
     $loading = true;
     try {
@@ -29,6 +34,20 @@ async function load()
     }
     return report;
 }
+
+function onUpdateClick()
+{
+    justMounted = false;
+    $svpage.url.searchParams.set('date_from', date_from);
+    $svpage.url.searchParams.set('date_to', date_to);
+    $svpage.url.searchParams.set('group_by', group_by);
+    goto(`?${$svpage.url.searchParams.toString()}`);
+}
+
+
+
+
+
 
 function processReport(rep: any)
 {
@@ -50,15 +69,25 @@ onMount(() => {
 <Title>Profit & Loss</Title>
 <Toolbar>
   
-    <div class="input-group input-group-sm" style="width: 15em;">
+    <div class="input-group input-group-sm" style="width: 11em;">
         <span class="input-group-text" id="basic-addon1">From</span>
-        <input type="date" class="form-control" placeholder="From" aria-label="From date" aria-describedby="From date">
+        <input type="date" class="form-control" bind:value={date_from} placeholder="From" aria-label="From date" aria-describedby="From date">
     </div>
-    <div class="input-group input-group-sm" style="width: 15em;">
+    <div class="input-group input-group-sm" style="width: 11em;">
         <span class="input-group-text" id="basic-addon1">To</span>
-        <input type="date" class="form-control" placeholder="To" aria-label="To date" aria-describedby="From date">
+        <input type="date" class="form-control" bind:value={date_to} placeholder="To" aria-label="To date" aria-describedby="From date">
     </div>
-    <Button width="10em" icon="bi-arrow-clockwise" size="sm" color="primary" on:click={()=> goto(`/app/finance/rules/edt`)}>Update</Button>
+    <div class="input-group input-group-sm" style="width: 11em;">
+        <span class="input-group-text" id="basic-addon1">Group by</span>
+        <select bind:value={group_by} class="form-select" aria-label="Group by select">
+            <option value="none" selected>None</option>
+            <option value="month">Month</option>
+            <option value="quarter">Quarter</option>
+            <option value="year">Year</option>
+            <option value="compare">Compare</option>
+        </select>
+    </div>
+    <Button width="8em" icon="bi-arrow-clockwise" busy={$loading} busytext="Updating" size="sm" color="primary" on:click={onUpdateClick} disabled={$loading}>Update</Button>
 
 
 
