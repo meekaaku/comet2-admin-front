26cab94bacdadac5c150fead3a51193615d22d9a
meekaaku <meekaaku@gmail.com>
2024-09-06 21:05:44 +0100

    Pagination improvements

--- src/routes/app/sales/orders/list/+page.svelte
+++ src/routes/app/sales/orders/list/+page.svelte
@@ -3,52 +3,69 @@ import { onMount, afterUpdate } from 'svelte';
 import { afterNavigate, goto } from '$app/navigation';
 import { page as svpage } from '$app/stores';
 import * as UI from '$lib/ui';
+import { Paginator } from '$lib/ui';
 import { comet, logger } from '$lib';
 import { loading } from '$lib/stores';
 import { formatNumber, formatAddress, formatDate, formatTime } from '$lib/utils';
 import type { ROrderListRow, RPaginated } from '$lib/types';
 
-let order_list: RPaginated<ROrderListRow>|undefined = undefined;
+let list: RPaginated<ROrderListRow>|undefined = undefined;
 let page = 1;
 let page_size = 5;
 let sort: string|undefined = undefined;
 let filters: any = undefined;
+let justMounted = false;
 
   
 
+/*
 function onPageChange(_page: number)
 {
     $svpage.url.searchParams.set('page', _page.toString());
     goto(`?${$svpage.url.searchParams.toString()}`);
 }
+*/
 
-async function loadOrders()
+function onPageChange({detail}: {detail: {page: number, page_size?: number}})
+{
+    justMounted = false;
+    const _page = detail.page;
+    const _page_size = detail.page_size || 100;
+    console.log(detail);
+    $svpage.url.searchParams.set('page', _page.toString());
+    $svpage.url.searchParams.set('page_size', _page_size.toString());
+    goto(`?${$svpage.url.searchParams.toString()}`);
+}
+
+
+
+async function loadList()
 {
     let query = new URLSearchParams($svpage.url.searchParams.toString());
     page = parseInt(query.get('page') || '1');
     $loading = true;
     try {
-      const list = await comet.orders.list({page, page_size, sort, filters});    
-      order_list = processList(list);
+      const _list = await comet.orders.list({page, page_size, sort, filters});    
+      list = processList(_list);
       $loading = false
     }
     catch(error) {
         logger.error(`Error loading orders: `, error)
         $loading = false
     }
-    return order_list;
+    return list;
 }
 
-function processList(list: RPaginated<ROrderListRow>)
+function processList(_list: RPaginated<ROrderListRow>)
 {
-    list.items.forEach(order => {
+    _list.items.forEach(order => {
         const lines = order.shipto_address.split('\n');
         if(!order.data)  {
           order.data = {}
         }
         order.data.shipto_lines = lines;
     })
-    return list;
+    return _list;
 }
 /*
 afterNavigate(async () => {
@@ -57,13 +74,17 @@ afterNavigate(async () => {
 })
 */
 
-onMount(async () => {
-  await loadOrders();
-  afterNavigate(async () => {
-    console.log('running afternavigate')
-    await loadOrders();
-  })
+afterNavigate(() => {
+    if(justMounted) return;
+    loadList();
 });
+
+onMount(() => {
+    justMounted = true;
+    loadList();
+});
+
+
 
 </script>
 
@@ -76,7 +97,7 @@ onMount(async () => {
 
 <div class="sticky-top">Toolar</div>
 
-{#if $loading|| !order_list}
+{#if $loading|| !list}
     Loading orders
 {:else}
 
@@ -95,8 +116,8 @@ onMount(async () => {
       </tr>
     </thead>
     <tbody>
-        {#if order_list}
-        {#each order_list.items as order}
+        {#if list}
+        {#each list.items as order}
         <tr>
             <td data-label="Order #" class="text-center">{order.order_no}</td>
             <td data-label="Date" class="text-center">{formatDate(order.date_created)} {formatTime(order.date_created)}</td>
@@ -122,7 +143,7 @@ onMount(async () => {
             <td data-label="Action" class="text-center">
                 <a on:click={comet.orders.get(order.order_id)}>View</a>
                 
-                , Ccanel
+                , Cancel
 
             </td>
         </tr>
@@ -131,27 +152,12 @@ onMount(async () => {
       <!-- Add more rows as needed -->
     </tbody>
   </table>
-<div class="d-flex justify-content-center mt-2">
-<nav aria-label="Page navigation example">
-  <ul class="pagination" style="display:contents">
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Previous" class:disabled={page === 1} on:click={()=> onPageChange(page-1)}>
-        <span aria-hidden="true">&laquo;</span>
-      </a>
-    </li>
-    {#each Array(order_list.page_count) as _, page0}
-    <li class="page-item" class:active = {page0 + 1 === order_list.page}>
-        <a class="page-link" href="#" on:click={()=> onPageChange(page0+1)}>{page0+1}</a>
-    </li>
-    {/each}
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Next" class:disabled={page === order_list.page_count} on:click={()=> onPageChange(page+1)}>
-        <span aria-hidden="true">&raquo;</span>
-      </a>
-    </li>
-  </ul>
-</nav>
-</div>
+
+  <div class="d-flex justify-content-center mt-2">
+    <Paginator page={list.page} page_count={list.page_count} page_size={list.page_size} on:pagechange={onPageChange} />
+  </div>
+
+
 {/if}
 
 
