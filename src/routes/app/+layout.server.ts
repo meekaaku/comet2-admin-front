ac04189afff346aea5598c9875bcaaaff69f003d
meekaaku <meekaaku@gmail.com>
2024-11-12 13:52:10 +0000

    Small fix

--- src/routes/app/+layout.server.ts
+++ src/routes/app/+layout.server.ts
@@ -2,8 +2,8 @@
 
 export async function load()
 {
-    console.log (process.env.BUILD);
+    console.log (process.env.VITE_BUILD);
 	return {
-        build: process.env.BUILD
+        build: process.env.VITE_BUILD
 	};
 }
