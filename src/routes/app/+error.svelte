f54c9b958b4dd69a085789e9b78af41f31477479
meekaaku <meekaaku@gmail.com>
2025-02-14 11:46:50 +0000

    Added better error handling page

--- src/routes/app/+error.svelte
+++ src/routes/app/+error.svelte
@@ -2,4 +2,21 @@
 	import { page } from '$app/stores';
 </script>
 
-This is error page
+
+	<div class="outer">
+		<div class="alert alert-danger text-center" role="alert">
+			<h1>{$page.error?.status}</h1>
+			{$page.error?.message}
+		</div>
+	</div>
+
+<style>
+	.outer {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: calc(100vh - 8em);
+	}
+</style>
+
--- src/routes/app/admin/roles/list/+page.server.ts
+++ src/routes/app/admin/roles/list/+page.server.ts
@@ -1,3 +1,4 @@
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { comet } from '$lib';
 
@@ -8,8 +9,14 @@ export async function load({url})
     const order_by = url.searchParams.get('order_by') || 'name';
     const order_dir = url.searchParams.get('order_dir') || 'asc';
     const search = url.searchParams.get('search') || '';
-    const list = await comet.admin.accessControl().list({page, page_size, order_by, order_dir, search});
-    return { list };
+    try {
+        const list = await comet.admin.accessControl().list({page, page_size, order_by, order_dir, search});
+        return { list };
+    } catch (err: any) {
+
+        console.error('we are here', err);
+        error(err.status, {message: err.message, status: err.status})
+    }
 };
 
 
